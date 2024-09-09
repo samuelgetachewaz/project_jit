2585069a8f5f758452a2418991ef95255958d688
samuelgetachewaz <bboysami063@gmail.com>
2024-09-10 00:53:20 +0300

    structuring-folders

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
 import React from 'react'
-
+import {LandingPage,AdminDashboard, ManageUsers,EnrolledCourses, ExamResults, QuestionBanks, ReferenceBooks, UploadedAssignments,UploadAssignments, GradeAssignments, ViewAssignments} from './src/pages/index';
+import { Route, Router, Routes } from 'react-router-dom';
 const app = () => {
   return (
-    <div>
-      
-    </div>
+    <Router>
+      <Routes>
+        <Route>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/exam-results" element={<ExamResults />} />
+        <Route path="/upload-assignment" element={<UploadAssignments />} />
+        
+        {/* Teacher routes */}
+        <Route path="/grade-assignments" element={<GradeAssignments />} />
+        <Route path="/view-submitted-assignments" element={<ViewAssignments />} />
+        <Route path="/add-exam-results" element={<ExamResults />} />
+        
+        {/* Admin routes */}
+        <Route path="/admin/dashboard" element={<AdminDashboard />} />
+        <Route path="/admin/manage-users" element={<ManageUsers />} />
+        </Route>
+      </Routes>
+    </Router>
   )
 }
 
